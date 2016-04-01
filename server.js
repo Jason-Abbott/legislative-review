dbc26226218355ed73e1284c03b705c2d3394f8f
Jason Abbott <dev@trailimage.com>
2016-04-01 14:55:23 -0600

    page renders

--- server.js
+++ server.js
@@ -2,30 +2,19 @@
 
 // Node Express application
 
-require('dotenv').config();
-
-const G = require('./lib');
+const L = require('./lib');
 const express = require('express');
-const massive = require('massive');
 const mode = process.env['NODE_ENV'];
+const path = require('path');
 const port = process.env['PORT'] | 3000;
+const root = { root: path.join(__dirname, './dist') };
 
-console.info('Connecting to PostgreSQL');
+L.app = express();
+L.app.use(require('./lib/middleware/json-response'));
+L.app.use(express.static(__dirname + '/dist'));
+L.app.use((req, res) => { res.sendFile('app.html', root) });
 
-massive.connect({ connectionString: G.config.db.connectionString }, (err, db) => {
-	// database structure is parsed so the callback takes a moment
-	if (err === null) {
-		G.db = db;
-	} else {
-		console.error(err);
-	}
-
-	G.app = express();
-	G.app.use(require('./lib/middleware/json-response'));
-	G.app.use(express.static(__dirname + '/dist'));
-
-	// initialize routes
-	require('./lib/sections/api-routes');
-	console.info("Starting JSON-Services in %s mode on port %s", mode, port);
-	G.app.listen(port);
-});
+// initialize routes
+//require('./lib/sections/api-routes');
+console.info("Starting Legislative Review service in %s mode on port %s", mode, port);
+L.app.listen(port);
