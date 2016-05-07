6a1f3a9880cd61634afe67f0e59ab709d635782e
Jason Abbott <dev@trailimage.com>
2016-05-07 14:50:14 -0600

    line calculations

--- test/tasks/pdf-to-html.test.js
+++ test/tasks/pdf-to-html.test.js
@@ -29,18 +29,23 @@ describe('PDF to HTML converter', ()=> {
 	
 	it('identifies lines as strikethrough or underline', ()=> {
 		// https://github.com/modesty/pdf2json#page-object-reference
-		let page = { HLines: [
-			{ x: 32.123, y: 35.822, w: 0.822, l: 1.644 },
-			{ x: 6.26, y: 36.884, w: 0.822, l: 3.699 }
-		]};
+		let page = {
+			Height: 49.5,
+			HLines: [
+				{ x: 32.123, y: 35.822, w: 0.822, l: 1.644 },
+				{ x: 6.26, y: 36.884, w: 0.822, l: 3.699 }
+			]
+		};
 		let el1 = { x: 31.872999999999998, y: 35.226, w: 26.301 };
-		let el2 = { x: 6.01, y: 35.98,	w: 59.178 };
+		let el2 = { x: 6.01, y: 35.98, w: 59.178 };
 
 		expect(pdfToHtml.isStricken(el1, page)).is.true;
 		expect(pdfToHtml.isUnderlined(el2, page)).is.true;
+		expect(pdfToHtml.isStricken(el2, page)).is.false;
+		expect(pdfToHtml.isUnderlined(el1, page)).is.false;
 	});
 
-	it('creates well-formed HTML', done => {
+	it.skip('creates well-formed HTML', done => {
 		let root = path.join(__dirname, '../');
 		let pending = 2;
 		let source = '';
