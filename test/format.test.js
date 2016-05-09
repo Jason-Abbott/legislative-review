6a739cb55ce0c499a4185c223d16eab50520ff5d
Jason Abbott <dev@trailimage.com>
2016-05-09 15:06:44 -0600

    parse test passing

--- test/format.test.js
+++ test/format.test.js
@@ -16,9 +16,22 @@ describe('String Formatter', ()=> {
 		expect(format.curlyQuotes(source)).equals(target);
 
 	});
+	it('removes extra spaces', ()=> {
+		expect(format.removeExtraSpace('one , two, three')).equals('one, two, three');
+		// remove space between block tags
+		expect(format.removeExtraSpace('<div>one</div> <div>two</div>')).equals('<div>one</div><div>two</div>');
+		// retain space between inline tags
+		expect(format.removeExtraSpace('<ins>one</ins> <del>two</del>')).equals('<ins>one</ins> <del>two</del>');
+		// leading tag space
+		expect(format.removeExtraSpace('<p attr="one"> two')).equals('<p attr="one">two');
+	});
+	it('normalize dashes', ()=> {
+		expect(format.dashes('em-dash --   after')).equals('em-dash &mdash; after');
+		expect(format.dashes('some- thing')).equals('some-thing');
+	});
 	it('removes redundant HTML tags', ()=> {
-		const source = '<del>something</del> <del>something else</del>';
-		const target = '<del>something something else</del>';
+		const source = '<del>one</del> <del>two three</del>';
+		const target = '<del>one two three</del>';
 		expect(format.removeExtraTags(source)).equals(target);
 	});
 	it('replaces straight with curly apostrophes', ()=> {
